131899f642d035e736d35f8d3937e0a2e1f9c3dc
jvaarala <jesse.vaarala@gmail.com>
2019-04-22 15:00:33 +0300

    sidebar proto

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,27 @@
-let i = 100;
+const suodatusButton = document.getElementById('suodatusButton');
+const suodattimetContent = document.getElementById('suodattimetContent');
+
+suodatusButton.addEventListener('click', suodattimetToggle);
+
+function suodattimetToggle() {
+  if (suodattimetContent.style.display === 'block')
+    suodattimetContent.style.display = 'none';
+  else
+    suodattimetContent.style.display = 'block';
+}
+
+function haeSuodattimet() {
+  for (let i = 0; i < 5; i++) {
+    const suodatinListItem = document.createElement('li');
+    const suodatinLabel = document.createElement('label');
+    const suodatinCheckbox = document.createElement('input');
+    suodatinCheckbox.type = 'checkbox';
+    suodatinCheckbox.id = 'suodatin' + i;
+    suodatinLabel.innerText = 'Suodatin #' + i; // innerTextiin APIlta saatu tieto suodattimista!
+    suodatinLabel.appendChild(suodatinCheckbox);
+    suodatinListItem.appendChild(suodatinLabel);
+    suodattimetContent.appendChild(suodatinListItem);
+  }
+}
+
+haeSuodattimet();
